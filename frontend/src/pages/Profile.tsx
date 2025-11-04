4ed3f038272afd47a03a876f6d48e00d1424b857
cjovignot <cosme.jovignot@epitech.eu>
2025-11-04 21:35:46 +0100

    [WIP] Mobile Navigation

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -3,10 +3,8 @@ import PageWrapper from "../components/PageWrapper";
 const Profile = () => {
   return (
     <PageWrapper>
-      <div className="p-6">
-        <h1 className="mb-4 text-2xl font-bold">👤 Profil</h1>
-        <p>Informations utilisateur ou préférences.</p>
-      </div>
+      <h1 className="!text-4xl">Profil</h1>
+      <p>Informations utilisateur ou préférences.</p>
     </PageWrapper>
   );
 };
