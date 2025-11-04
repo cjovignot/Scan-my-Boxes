dcc27a673af0ff4477c795e43e6db0e4dfa36735
cjovignot <cosme.jovignot@epitech.eu>
2025-11-04 06:41:39 +0100

    [WIP] Mobile Navigation

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -1,14 +1,18 @@
 import { NavLink, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
-import { Home, User, Plus } from "lucide-react";
+import { Home, User, ScanQrCode } from "lucide-react";
 
 const BottomNav = () => {
   const location = useLocation(); // 👈 On récupère la route active
   const isHome = location.pathname === "/"; // Vérifie si on est sur "/"
 
   const navItems = [
-    { to: "/", icon: <Home size={22} />, label: "Accueil" },
-    { to: "/profile", icon: <User size={22} />, label: "Profil" },
+    { to: "/", icon: <Home size={22} strokeWidth={0.75} />, label: "Accueil" },
+    {
+      to: "/profile",
+      icon: <User size={22} strokeWidth={0.75} />,
+      label: "Profil",
+    },
   ];
 
   const handleFabClick = () => {
@@ -17,44 +21,46 @@ const BottomNav = () => {
   };
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-around py-2 bg-white border-t shadow-md h-14 md:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-around h-auto py-2 bg-gray-800 shadow-md md:hidden">
       {/* Icônes de gauche */}
       {navItems.slice(0, 2).map((item) => (
         <NavLink
           key={item.to}
           to={item.to}
           className={({ isActive }) =>
-            `flex flex-col items-center text-xs transition-colors ${
-              isActive ? "text-blue-600" : "text-gray-500"
+            `flex flex-col items-center text-xs transition-colors !font-thin ${
+              isActive ? "!text-yellow-400" : "!text-gray-400"
             }`
           }
         >
           {({ isActive }) => (
             <motion.div
-              whileTap={{ scale: 0.85 }}
+              // whileTap={{ scale: 0.85 }}
               animate={{ scale: isActive ? 1.2 : 1 }}
               transition={{ type: "spring", stiffness: 300, damping: 15 }}
               className="flex flex-col items-center"
             >
               {item.icon}
-              <span className="text-[10px] mt-1">{item.label}</span>
+              {!isActive && (
+                <span className="text-[10px] mt-1">{item.label}</span>
+              )}
             </motion.div>
           )}
         </NavLink>
       ))}
 
-      {isHome && (
+      {
         <motion.button
           whileTap={{ scale: 0.9 }}
-          initial={{ scale: 0, opacity: 0, y: 10 }}
+          // initial={{ scale: 0, opacity: 0, y: 10 }}
           animate={{ scale: 1, opacity: 1, y: 0 }}
           transition={{ type: "spring", stiffness: 260, damping: 20 }}
           onClick={handleFabClick}
-          className="fixed !p-2  h-auto w-auto text-white bottom-7 !bg-blue-600 !rounded-full"
+          className="fixed !p-3  h-auto w-auto text-white bottom-7 !bg-gray-900 !rounded-full"
         >
-          <Plus size={32} />
+          <ScanQrCode size={32} />
         </motion.button>
-      )}
+      }
 
       {/* Icônes de droite */}
       {navItems.slice(2).map((item) => (
