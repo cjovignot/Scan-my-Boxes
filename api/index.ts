e2b643dbeb5e132ca1ae5f15209e5d62e5c06643
cjovignot <cosme.jovignot@epitech.eu>
2025-11-06 13:23:10 +0100

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     res.setHeader("Vary", "Origin");
   }
 
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT, PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
