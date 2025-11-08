df1b7a3819e6f38c84168f709233c1598e26158f
cjovignot <cosme.jovignot@epitech.eu>
2025-11-08 22:40:53 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
+import Storages from "./pages/Storages";
 import AuthSuccess from "./pages/AuthSuccess";
 import { AuthProvider } from "./contexts/AuthProvider";
 import { useEffect } from "react";
@@ -56,6 +57,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<Admin />} />
+            <Route path="/storages" element={<Storages />} />
             <Route path="/auth/success" element={<AuthSuccess />} />
           </Route>
         </Routes>
