49056208b5b74a1c203131270e29ef2206a45188
cjovignot <cosme.jovignot@epitech.eu>
2025-11-08 22:39:41 +0100

    Create Storages.tsx

---
+++ frontend/src/pages/Storages.tsx
@@ -0,0 +1,24 @@
+import { useApi } from "../hooks/useApi";
+import PageWrapper from "../components/PageWrapper";
+
+const Storages = () => {
+  const { data, loading, error } = useApi<{ message: string }>("/api/example");
+
+  return (
+    <PageWrapper>
+      <div className="flex flex-col items-center min-h-screen px-6 py-10 text-white bg-gray-950">
+        {/* Titre principal */}
+        <h1 className="mb-6 text-4xl font-bold text-center text-yellow-400">
+          Entrepôts
+        </h1>
+
+        {/* Note ou footer */}
+        <p className="mt-10 text-sm text-center text-gray-500">
+Liste de vos entrepôts.
+        </p>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default Storages;
