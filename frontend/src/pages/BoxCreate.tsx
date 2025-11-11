12b80ec495a54a76d69e8abf5408dae367730496
cjovignot <cosme.jovignot@epitech.eu>
2025-11-11 06:22:28 +0100

    [FIX] Upload image item box

--- frontend/src/pages/BoxCreate.tsx
+++ frontend/src/pages/BoxCreate.tsx
@@ -99,20 +99,27 @@ const BoxCreate = () => {
     updated[index].uploading = true;
     setContentItems(updated);
 
-    const formData = new FormData();
-    formData.append("image", file);
-
     try {
-      const res = await fetch("/api/upload", {
-        method: "POST",
-        body: formData,
-      });
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/${
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
       const data = await res.json();
 
-      if (data.url) {
-        updated[index].picture = data.url;
+      if (data.secure_url) {
+        updated[index].picture = data.secure_url;
       } else {
-        alert("❌ Erreur lors de l'upload de l’image.");
+        console.error("Réponse Cloudinary invalide :", data);
+        alert("❌ Erreur lors de l’upload de l’image.");
       }
     } catch (err) {
       console.error(err);
