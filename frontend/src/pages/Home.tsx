3267c5b4c50c7c730bc35f8e468593944bf65726
cjovignot <cosme.jovignot@epitech.eu>
2025-11-18 12:50:17 +0100

    [UPD] Dashboard SAAS design

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -184,7 +184,7 @@ const Dashboard = () => {
           </h1>
         </motion.div>
 
-<div className="grid grid-cols-2 gap-5 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+<div className="grid grid-cols-2 gap-3 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
   {stats.map(({ id, label, value, description, icon: Icon }) => (
     <motion.div
       key={id}
